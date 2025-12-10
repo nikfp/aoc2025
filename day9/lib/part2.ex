849adfc2807a6afdc7a48e2f300a1a06eb178e6b
Nik FP <nik@nikfp.com>
2025-12-10 10:15:32 -0700

    day 9 part 2 test passing but too slow

--- day9/lib/part2.ex
+++ day9/lib/part2.ex
@@ -5,19 +5,12 @@ defmodule Part2 do
       input
       |> Enum.map(&List.to_tuple(&1))
 
-    edge_coords =
-      input
-      |> Enum.chunk_every(2, 1, input)
-      |> Enum.flat_map(fn [left, right] -> get_edge_coords(left, right) end)
-      |> MapSet.new()
-
     edge_list =
       input
       |> Enum.chunk_every(2, 1, input)
       |> Enum.map(fn [left, right] ->
-        {left, right, get_edge_coords(left, right) |> MapSet.new()}
+        get_edge_coords(left, right) |> MapSet.new()
       end)
-      |> IO.inspect()
 
     # then get the list from part 1, starting from the top
     indexed_input =
@@ -38,37 +31,47 @@ defmodule Part2 do
     # for each pair, get all 4 edges and check that every edge
     # doesn't cross out of bounds
     Enum.find(sorted_coords, fn {_, {left, right}} ->
-      rect_fits_in_bounds?(left, right, edge_coords, edge_list)
+      rect_fits_in_bounds?(left, right, edge_list)
     end)
   end
 
-  defp rect_fits_in_bounds?({x1, y1}, {x2, y2}, all_edge_coords, edge_list) do
-        # get all 4 edges of each test rectangle and a set of the edges of the main polygon. 
+  defp rect_fits_in_bounds?({x1, y1}, {x2, y2}, edge_list) do
+    # get all 4 edges of each test rectangle and a set of the edges of the main polygon. 
     [
       {{x1, y1}, {x2, y1}},
       {{x1, y1}, {x1, y2}},
       {{x2, y2}, {x1, y2}},
       {{x2, y2}, {x2, y1}}
     ]
+    |> Enum.reduce_while(true, fn {left, right}, _acc ->
+      edge_set =
+        get_edge_coords(left, right)
+        |> MapSet.new()
 
-    # |> Enum.reduce_while(true, fn {left, right}, acc ->
-    #   edge_coords =
-    #     get_edge_coords(left, right)
-    #     |> MapSet.new()
-    #
-    #   if(MapSet.disjoint?(edge_coords, all_edge_coords)) do
-    #     # Then check each test edge and if it intersects the main polygon, 
-    #     {:cont, acc}
-    #   else
-    #     # then switch to testing individual polygon edges. 
-    #     # If the test edge touches a polygon edge at exactly 1 point 
-    #     Enum.find(edge_list, fn {_, })
-    #     # and that point is not an endpoint of the test edge, that's a crossing, 
-    #     # which indicates the test edge runs outside the main polygon. 
-    #     # We can assume all corners are inside the polygon to start, 
-    #     # so if no edges test as crossing, the test rectangle is inside the polygon.
-    #   end
-    # end)
+      edge_inner =
+        MapSet.difference(edge_set, MapSet.new([left, right]))
+
+      potential_crossed_edges =
+        edge_list
+        |> Enum.filter(fn edge ->
+          MapSet.intersection(edge, edge_inner) |> MapSet.size() > 0
+        end)
+
+      Enum.all?(potential_crossed_edges, fn el ->
+        MapSet.intersection(el, edge_set) |> MapSet.size() > 1
+      end)
+      |> case do
+        true -> {:cont, true}
+        false -> {:halt, false}
+      end
+    end)
+
+    # then switch to testing individual polygon edges. 
+    # If the test edge touches a polygon edge at exactly 1 point 
+    # and that point is not an endpoint of the test edge, that's a crossing, 
+    # which indicates the test edge runs outside the main polygon. 
+    # We can assume all corners are inside the polygon to start, 
+    # so if no edges test as crossing, the test rectangle is inside the polygon.
   end
 
   defp get_edge_coords({x1, y1}, {x2, y2}) do
