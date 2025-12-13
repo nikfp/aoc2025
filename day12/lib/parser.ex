233714e0af7e67ae72712549e921fee6db76afa6
Nik FP <nik@nikfp.com>
2025-12-12 17:54:09 -0700

    day 12 part 1

--- day12/lib/parser.ex
+++ day12/lib/parser.ex
@@ -1,6 +1,45 @@
 defmodule Parser do
   def parse(file) do
-    IO.puts("Parser not implemented yet")
-    file
+    [boxes | shapes] =
+      file
+      |> File.read!()
+      |> String.split("\n\n", trim: true)
+      |> Enum.reverse()
+
+    boxes =
+      boxes
+      |> String.split("\n", trim: true)
+      |> Enum.map(fn line ->
+        [size, index_list] =
+          line
+          |> String.split(":", trim: true)
+
+        size =
+          String.split(size, "x", trim: true)
+          |> Enum.map(&String.to_integer/1)
+          |> Enum.product()
+
+        index_list =
+          index_list
+          |> String.split(" ", trim: true)
+          |> Enum.map(&String.to_integer/1)
+          |> Enum.with_index()
+
+        %{size: size, index_list: index_list}
+      end)
+
+    shapes =
+      shapes
+      |> Enum.reduce(%{}, fn shape, acc ->
+        [index, positions] = String.split(shape, ":", trim: true)
+
+        index = String.to_integer(index)
+
+        positions = String.count(positions, "#")
+
+        Map.put(acc, index, positions)
+      end)
+
+    [shapes, boxes]
   end
 end
--- day12/lib/part1.ex
+++ day12/lib/part1.ex
@@ -1,6 +1,19 @@
 defmodule Part1 do
   def solve(input) do
-    IO.puts("Part 1 solver not implemented yet")
-    input
+    [shapes, boxes ] = input
+    
+    boxes 
+    |> Enum.filter(fn %{size: size, index_list: ilist} -> 
+      ilist
+      |> Enum.map(fn {count, index} -> 
+        count * Map.get(shapes, index)
+      end)
+      |> Enum.sum()
+      |> case do
+        val when val > size -> false
+        _val -> true
+      end
+    end)
+    |> Enum.count()
   end
 end
